1519203f97bd39edd2e4d655d453f18cedebd0e4
Neil Devas <neil.devas97@gmail.com>
2021-03-07 14:20:19 -0500

    Add card background image for neildevas.com

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -3,6 +3,7 @@ import Img from "gatsby-image"
 import Spacer from "../components/Spacer"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
+import BackgroundImage from 'gatsby-background-image'
 
 const Employment = () => {
   const data = useStaticQuery(graphql`
@@ -19,9 +20,9 @@ const Employment = () => {
                       image {
                           publicURL
                       }
-                      backgroundImage {
+                      cardBackgroundImage {
                           childImageSharp {
-                              fluid {
+                              fluid(quality:100) {
                                   ...GatsbyImageSharpFluid
                               }
                           }
@@ -31,7 +32,6 @@ const Employment = () => {
           }
       }
   `);
-
   return (
     <div>
       <div className={'full-width-section'}>
@@ -53,20 +53,35 @@ const Employment = () => {
 
 export default Employment;
 
-const ProjectCard = ({ backgroundColor, slug, textColor, title, logoUrl, shouldConstrainLogo, backgroundImage }) => {
+const ProjectCard = ({ backgroundColor, slug, textColor, title, logoUrl, shouldConstrainLogo, cardBackgroundImage }) => {
   const imgStyle = { // TODO - not sustainable. Figure out a better solution with GraphQL
     overflow: "hidden",
     maxWidth: 200,
     ...(shouldConstrainLogo ? { width: '50%', maxWidth: 150 } : {})
   }
-  return (
-    <Link to={`/projects/${slug}`} class={'flex justify-center'}>
-      <div class={'project-card'} style={{ backgroundColor }}>
-        <div class={'flex-1 flex justify-center align-center'}>
+  const renderComponent = () => {
+    if (cardBackgroundImage) {
+      return (
+        <BackgroundImage fluid={cardBackgroundImage.childImageSharp.fluid} className={'project-card relative'} style={{ overflow: 'hidden'}}>
+          <div class={'card-background-gradient absolute inset-0'} />
+            <div className={'flex-1'}/>
+            <h4 style={{ color: textColor }} className={'text-center z-10'}>{title}</h4>
+        </BackgroundImage>
+      )
+    }
+    return (
+      <div className={'project-card'} style={{ backgroundColor }}>
+        <div className={'flex-1 flex justify-center align-center'}>
           <img src={logoUrl} style={imgStyle}/>
         </div>
-        <h4 style={{ color: textColor }} class={'text-center'}>{title}</h4>
+        <h4 style={{ color: textColor }} className={'text-center'}>{title}</h4>
       </div>
+    )
+  }
+
+  return (
+    <Link to={`/projects/${slug}`} class={'flex justify-center'}>
+      {renderComponent()}
     </Link>
   )
 }
